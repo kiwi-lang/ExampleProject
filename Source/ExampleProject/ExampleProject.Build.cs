5e6fafed8a73807d5e067a109d0ba36272dec976
Setepenre <pierre.delaunay@outlook.com>
2023-02-16 05:28:06 -0500

    Add version info

--- Source/ExampleProject/ExampleProject.Build.cs
+++ Source/ExampleProject/ExampleProject.Build.cs
@@ -8,6 +8,26 @@ public class ExampleProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara" });
+        PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+			"NavigationSystem",
+			"AIModule",
+			"Niagara"
+		});
+
+		        // Version Info
+        // ------------
+        // Automatically set by the CI
+        string EXAMPLEPROJECT_TAG = "v1.2.3";
+        string EXAMPLEPROJECT_HASH = "54b907fdc";
+        string EXAMPLEPROJECT_DATE = "2024-02-16 11:28:06 -0500";
+
+        PublicDefinitions.Add("EXAMPLEPROJECT_TAG=" + EXAMPLEPROJECT_TAG);
+        PublicDefinitions.Add("EXAMPLEPROJECT_COMMIT=" + EXAMPLEPROJECT_HASH);
+        PublicDefinitions.Add("EXAMPLEPROJECT_DATE=" + EXAMPLEPROJECT_DATE);
     }
 }
